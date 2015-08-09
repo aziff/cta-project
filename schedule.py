b7d8970dff3a392246afaa9a5677f91769d32c3d
Anna Ziff <aziff@uchicago.edu>
2015-08-09 12:27:08 -0500

    need to fix data type

--- schedule.py
+++ schedule.py
@@ -1,14 +1,17 @@
 import csv
+import numpy
 
 
 def import_csv():
   data = []
   r = csv.reader(open('171schedule.csv','rt',newline=''), dialect='excel')
   for l in r:
-    data.append(l)  
+    data.append(l)
+  
   return data
 
-      
-      
+    
+     
 if __name__ == '__main__':
-  import_csv()
+  data = import_csv()
+  print(data)
