deebc9824c66465703c4de62c16018e911437310
Anna Ziff <aziff@uchicago.edu>
2016-02-14 17:14:13 -0600

    attempted to make two plots...buggy

--- code/animation/circles/circle_animation.py
+++ code/animation/circles/circle_animation.py
@@ -36,28 +36,45 @@ FILE_PATH = '../../../data/processed/'
 
 if __name__ == '__main__':
   data = load_data()
+
   for r in data:
     data[r] = get_distance(data[r])
-    
+
+  # Get stops and points
   stops171, stops172 = route_info(data['171']), route_info(data['172'])
   points171, points172 = circle_points(stops171), circle_points(stops172)
-  
+
+  plt.figure(1)
+
   #Set figure
-  fig = plt.figure()
+  fig172 = plt.subplot(212)
   plt.axes().set_aspect('equal', 'datalim')
-  
+
   # Add circle
   circle1=plt.Circle((0,0),1,color='black',fill=False)
   plt.gcf().gca().add_artist(circle1)
-  
+
   # Add stops
   plt.scatter(points172[0]['x'], points172[0]['y'], s=40, c='black')
-  
-  
-  for i in data['172']:
+
+
+  for x, y, i in zip(points172[0]['x'], points172[0]['y'], data['172']):
     txt = data['172'][i][1]
-    for x, y in zip(points172[0]['x'], points172[0]['y']):
-      plt.annotate(txt, xy=(x,y))
+    plt.annotate(txt, xy=(x,y))
+
+  fig171 = plt.subplot(212)
+  plt.axes().set_aspect('equal', 'datalim')
+
+  # Add circle
+  circle1=plt.Circle((0,0),1,color='black',fill=False)
+  plt.gcf().gca().add_artist(circle1)
+
+  # Add stops
+  plt.scatter(points171[0]['x'], points171[0]['y'], s=40, c='black')
+
+
+  for x, y, i in zip(points171[0]['x'], points171[0]['y'], data['171']):
+    txt = data['171'][i][1]
+    plt.annotate(txt, xy=(x,y))
 
   plt.show()
-
